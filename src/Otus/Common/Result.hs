40137b38fbd9ffa06689d22c7da095259312d647
dark-flames <github@dark-flames.dev>
2026-03-20 18:01:17 +0000

    common: traversable result

--- src/Otus/Common/Result.hs
+++ src/Otus/Common/Result.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Otus.Common.Result (
   Result (..),
   ResultT (..),
 ) where
 
 import Control.Monad.Error.Class
+import Control.Monad.Trans.Class
 
 data Result e a
   = Success a
@@ -28,6 +31,25 @@ instance MonadError e (Result e) where
   throwError = Failure
   catchError (Failure e) handler = handler e
   catchError (Success a) _ = Success a
+
+instance Foldable (Result e) where
+  foldMap f = \case
+    (Failure _) -> mempty
+    (Success a) -> f a
+
+  foldr f z = \case
+    (Failure _) -> z
+    (Success a) -> f a z
+
+  length = \case
+    (Failure _) -> 0
+    (Success _) -> 1
+
+instance Traversable (Result e) where
+  traverse :: (Applicative f) => (a -> f b) -> Result e a -> f (Result e b)
+  traverse f = \case
+    (Failure e) -> pure $ Failure e
+    (Success a) -> Success <$> f a
 
 newtype ResultT e m a = ResultT
   { runResultT :: m (Result e a)
@@ -57,3 +79,21 @@ instance (Monad m) => MonadError e (ResultT e m) where
       runResultT ra >>= \case
         Failure e -> runResultT $ h e
         Success r -> pure $ Success r
+
+instance MonadTrans (ResultT e) where
+  lift :: (Monad m) => m a -> ResultT e m a
+  lift = ResultT . fmap Success
+
+instance (Foldable m) => Foldable (ResultT e m) where
+  foldMap :: (Monoid n) => (a -> n) -> ResultT e m a -> n
+  foldMap f r = foldMap (foldMap f) $ runResultT r
+
+  foldr f z r = foldr f' z $ runResultT r
+    where
+      f' r' b = foldr f b r'
+
+instance (Traversable m) => Traversable (ResultT e m) where
+  traverse
+    :: (Applicative f)
+    => (a -> f b) -> ResultT e m a -> f (ResultT e m b)
+  traverse f r = ResultT <$> traverse (traverse f) (runResultT r)
